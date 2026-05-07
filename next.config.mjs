423d04120770fcea663f198289bcbb381c23bdf3
KingBoomBoom <52889218+KingBoomBoom@users.noreply.github.com>
2026-05-07 09:51:35 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    typescript: {
-        ignoreBuildErrors: true,
-    }
+  async rewrites() {
+    return [
+      {
+        // 告诉 Vercel：只要网页请求 /api/oracle
+        source: '/api/oracle',
+        // Vercel 就去后台悄悄访问这个真实的 HTTPS 地址
+        destination: 'https://api.cleanstems.com/oracle_data.json',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
